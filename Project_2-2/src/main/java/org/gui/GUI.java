9b3f74bec7a9f19a2e151e7d448fe3bef7538c48
LucWehrenss <lj.wehrens@student.maastrichtuniversity.nl>
2024-04-10 12:52:56 +0200

    gui input screen done

---
+++ Project_2-2/src/main/java/org/gui/GUI.java
@@ -0,0 +1,45 @@
+package org.gui;
+
+import javax.swing.*;
+
+public class GUI {
+    private JFrame frame;
+    private JTextField userInput;
+    private JLabel label1, label2;
+    private JTextField day, month, year;
+
+    public GUI() {
+        frame = new JFrame("Weather Predictor");
+        frame.setSize(450, 200);
+        frame.setLayout(null);
+
+        label1 = new JLabel("Please input the date for which you would like to predict the weather:");
+        label1.setSize(400, 30);
+        label1.setLocation(20, 20);
+        frame.add(label1);
+
+        label2 = new JLabel("Please use the following format: dd/mm/yyyy, e.g. 01/01/2001");
+        label2.setSize(400, 40);
+        label2.setLocation(20, 40);
+        frame.add(label2);
+
+        day = new JTextField();
+        day.setSize(50, 30);
+        day.setLocation(110, 100);
+        frame.add(day);
+
+        month = new JTextField();
+        month.setSize(50, 30);
+        month.setLocation(170, 100);
+        frame.add(month);
+
+        year = new JTextField();
+        year.setSize(100, 30);
+        year.setLocation(230, 100);
+        frame.add(year);
+
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
