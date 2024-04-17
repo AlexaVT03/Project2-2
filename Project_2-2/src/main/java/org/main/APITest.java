6a6ffd143e52ab5ba59c16b916ca710924fde6f7
Jakub_Suszwedyk <suszwedykkuba@gmail.com>
2024-04-17 13:02:27 +0200

    fixed a  typo in API test

--- Project_2-2/src/main/java/org/main/APITest.java
+++ Project_2-2/src/main/java/org/main/APITest.java
@@ -1,8 +1,6 @@
 package org.main;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
